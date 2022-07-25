f41e61864d7a81a447217cd49dff959a92410646
bruce <lant@ghzq.com.cn>
2022-07-25 16:18:30 +0800

    完成支付模块2

--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,7 +1,16 @@
-package com.mmall.service;/**
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
+/**
  * @author bruce
  * 2022/7/16 18:00
  */
 
-    public interface IOrderService {
+public interface IOrderService {
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+    ServerResponse aliCallback(Map<String,String> params);
+    ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
 }
